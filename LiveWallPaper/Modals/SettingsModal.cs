29e358906358ca5859f0579c6f1bfaaa25cc7141
gs5a62 <b.ahmeed.h@gmail.com>
2021-01-19 18:29:08 +0300

    Add project files.

---
+++ LiveWallPaper/Modals/SettingsModal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LiveWallPaper.Modals
+{
+    class SettingsModal
+    {
+        public DateTime? LastAPICheck { get; set; }
+        public TimeSpan? Dawn { get; set; }
+        public TimeSpan? Sunrise { get; set; }
+        public TimeSpan? Sunset { get; set; }
+        public string LastAppliedImage { get; set; }
+    }
+}
